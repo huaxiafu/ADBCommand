89be13a91acad4da2e25efd68dad320adf56ad2f
huaxiafu <huaxiafu@163.com>
2024-05-25 13:20:43 +0800

    测试git

--- app/src/main/java/com/fzc/adbcommand/MainActivity.java
+++ app/src/main/java/com/fzc/adbcommand/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                         throw new RuntimeException(e);
                     }
                     try {
-                        Thread.sleep(3000);
+                        Thread.sleep(3000); //设置睡觉时间
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
